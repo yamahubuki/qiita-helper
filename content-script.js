16e1bc55f7b5a9ad2b5ec256efd69d1aba6da1e7
yamahubuki <itiro.ishino@gmail.com>
2022-12-18 17:03:27 +0900

    v0.0.2 テキストのフォントサイズを0に指定、ライセンス明示

--- content-script.js
+++ content-script.js
@@ -1,4 +1,4 @@
 const targets = document.getElementsByTagName("h2");
 Array.prototype.forEach.call(targets, function(item) {
-	item.children[1].innerText="見出し";
+	item.children[1].innerHTML='<span style="font-size:0">見出し</span>';
 });
