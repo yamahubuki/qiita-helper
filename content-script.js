6bac798680da43da448a570ae3dc6d6474084ff6
yamahubuki <itiro.ishino@gmail.com>
2022-12-20 10:56:16 +0900

    v0.0.3 h1見出し対応

--- content-script.js
+++ content-script.js
@@ -1,4 +1,11 @@
-const targets = document.getElementsByTagName("h2");
+let targets = document.getElementsByTagName("h1");
+Array.prototype.forEach.call(targets, function(item) {
+	if (item.childElementCount > 1) {
+		item.children[1].innerHTML='<span style="font-size:0">見出し</span>';
+	}
+});
+
+targets = document.getElementsByTagName("h2");
 Array.prototype.forEach.call(targets, function(item) {
 	item.children[1].innerHTML='<span style="font-size:0">見出し</span>';
 });
